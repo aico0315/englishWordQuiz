facd0365d73541bdc51fc3778b1e3687444795e1
aico0315 <aiko.phoenix12@gmail.com>
2026-01-30 16:51:55 +0900

    modularizationOfData

---
+++ js/data.js
@@ -0,0 +1,59 @@
+"use script";
+
+export const wordRecords = [
+  {
+    question: `Average`,
+    supplement:``,
+    answer: ["平均", "へいきん"],
+  },
+  {
+    question: `Addition`,
+    supplement:``,
+    answer: ["足し算", "たし算", "たしざん"],
+  },
+  {
+    question: `Subtraction
+    (ｻﾌﾞﾄﾗｸｼｮﾝ)`,
+    supplement:``,
+    answer: ["引き算", "ひき算", "ひきざん"],
+  },
+  {
+    question: `Multiplication
+    (ﾏﾙﾁﾌﾟﾘｹｰｼｮﾝ)`,
+    supplement:``,
+    answer: ["掛け算", "かけ算", "かけざん"],
+  },
+  {
+    question: `Division
+    (ﾃﾞｨｳﾞｨｼﾞｮﾝ)`,
+    supplement:``,
+    answer: ["割り算", "わり算", "わりざん"],
+  },
+  {
+    question: `Remainder
+    (ﾘﾒｲﾝﾀﾞｰ)`,
+    supplement:`Reminder(ﾘﾏｲﾝﾀﾞｰ)は「思い出させる、通知する」
+    ※似ているので注意する`,
+    answer: ["割り算の余り", "割り算のあまり", "わり算のあまり", "わりざんの余り", "わりざんのあまり"],
+  },
+  {
+    question: `String`,
+    supplement:`Characterは「文字」混同しないよう注意する`,
+    answer: ["文字列", "文字れつ", "もじ列", "もじれつ"],
+  },
+  {
+    question: `Character`,
+    supplement:`Stringは「文字列」混同しないよう注意する`,
+    answer: ["文字", "もじ"],
+  },
+  {
+    question: `Array`,
+    supplement:``,
+    answer: ["配列", "はいれつ"],
+  },
+  {
+    question: `Subject`,
+    supplement:`件名、科目、教科など`,
+    answer: ["件名", "科目", "教科", "けんめい", "かもく", "きょうか"],
+  },
+]
--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 "use strict";
 
+import { wordRecords } from './data.js';
+
 const questionArea = document.querySelector(".question-area");
 const counterArea = document.querySelector(".counter-area");
 const setQuestion = document.querySelector(".set-question");
@@ -21,63 +23,6 @@ const answerView = document.getElementById("answer-view");
 const clearView = document.getElementById("clear-view");
 
 
-const wordRecords = [
-  {
-    question: `Average`,
-    supplement:``,
-    answer: ["平均", "へいきん"],
-  },
-  {
-    question: `Addition`,
-    supplement:``,
-    answer: ["足し算", "たし算", "たしざん"],
-  },
-  {
-    question: `Subtraction
-    (ｻﾌﾞﾄﾗｸｼｮﾝ)`,
-    supplement:``,
-    answer: ["引き算", "ひき算", "ひきざん"],
-  },
-  {
-    question: `Multiplication
-    (ﾏﾙﾁﾌﾟﾘｹｰｼｮﾝ)`,
-    supplement:``,
-    answer: ["掛け算", "かけ算", "かけざん"],
-  },
-  {
-    question: `Division
-    (ﾃﾞｨｳﾞｨｼﾞｮﾝ)`,
-    supplement:``,
-    answer: ["割り算", "わり算", "わりざん"],
-  },
-  {
-    question: `Remainder
-    (ﾘﾒｲﾝﾀﾞｰ)`,
-    supplement:`Reminder(ﾘﾏｲﾝﾀﾞｰ)は「思い出させる、通知する」
-    ※似ているので注意する`,
-    answer: ["割り算の余り", "割り算のあまり", "わり算のあまり", "わりざんの余り", "わりざんのあまり"],
-  },
-  {
-    question: `String`,
-    supplement:`Characterは「文字」混同しないよう注意する`,
-    answer: ["文字列", "文字れつ", "もじ列", "もじれつ"],
-  },
-  {
-    question: `Character`,
-    supplement:`Stringは「文字列」混同しないよう注意する`,
-    answer: ["文字", "もじ"],
-  },
-  {
-    question: `Array`,
-    supplement:``,
-    answer: ["配列", "はいれつ"],
-  },
-  {
-    question: `Subject`,
-    supplement:`件名、科目、教科など`,
-    answer: ["件名", "科目", "教科", "けんめい", "かもく", "きょうか"],
-  },
-]
 
 //シャッフルされた問題
 const shuffledQuestions = [...wordRecords];
