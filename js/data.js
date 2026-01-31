5c58832fdd025ce7d074b5fea8c327629a9c59a0
aico0315 <aiko.phoenix12@gmail.com>
2026-01-31 18:18:01 +0900

    addQuestions

--- js/data.js
+++ js/data.js
@@ -12,28 +12,25 @@ export const wordRecords = [
     answer: ["足し算", "たし算", "たしざん"],
   },
   {
-    question: `Subtraction
-    (ｻﾌﾞﾄﾗｸｼｮﾝ)`,
-    supplement:``,
+    question: `Subtraction`,
+    supplement:`読み：ｻﾌﾞﾄﾗｸｼｮﾝ`,
     answer: ["引き算", "ひき算", "ひきざん"],
   },
   {
-    question: `Multiplication
-    (ﾏﾙﾁﾌﾟﾘｹｰｼｮﾝ)`,
-    supplement:``,
+    question: `Multiplication`,
+    supplement:`読み：ﾏﾙﾁﾌﾟﾘｹｰｼｮﾝ`,
     answer: ["掛け算", "かけ算", "かけざん"],
   },
   {
-    question: `Division
-    (ﾃﾞｨｳﾞｨｼﾞｮﾝ)`,
-    supplement:``,
+    question: `Division`,
+    supplement:`読み：ﾃﾞｨｳﾞｨｼﾞｮﾝ`,
     answer: ["割り算", "わり算", "わりざん"],
   },
   {
-    question: `Remainder
-    (ﾘﾒｲﾝﾀﾞｰ)`,
-    supplement:`Reminder(ﾘﾏｲﾝﾀﾞｰ)は「思い出させる、通知する」
-    ※似ているので注意する`,
+    question: `Remainder`,
+    supplement:`読み：ﾘﾒｲﾝﾀﾞｰ
+    「思い出させる、通知する」はReminder(ﾘﾏｲﾝﾀﾞｰ)
+    ※似ているので注意する。`,
     answer: ["割り算の余り", "割り算のあまり", "わり算のあまり", "わりざんの余り", "わりざんのあまり"],
   },
   {
@@ -55,5 +52,65 @@ export const wordRecords = [
     question: `Subject`,
     supplement:`件名、科目、教科など`,
     answer: ["件名", "科目", "教科", "けんめい", "かもく", "きょうか"],
+  },
+  {
+    question: `Semantics`,
+    supplement:`読み：ｾﾏﾝﾃｨｸｽ
+    内容や意図、動作など`,
+    answer: ["内容", "意図", "動作", "ないよう", "いと", "どうさ"],
+  },
+  {
+    question: `Syntax`,
+    supplement:`形式やルール、書き方など`,
+    answer: ["形式", "ルール", "書き方", "けいしき", "るーる", "かきかた"],
+  },
+  {
+    question: `delimiter`,
+    supplement:`読み：ﾃﾞﾐﾘﾀ
+    形式やルール、書き方など`,
+    answer: ["区切り文字", "区切り記号", "区切りもじ", "くぎり文字", "区切り記号", "区切りきごう", "くぎり記号"],
+  },
+  {
+    question: `specified`,
+    supplement:`読み：ｽﾍﾟｼﾌｧｲﾄﾞ
+    指定された、明記された`,
+    answer: ["指定された", "明記された", "していされた", "めいきされた"],
+  },
+  {
+    question: `integer`,
+    supplement:`整数`,
+    answer: ["整数", "せいすう"],
+  },
+  {
+    question: `vowel`,
+    supplement:`母音`,
+    answer: ["母音", "ぼいん"],
+  },
+  {
+    question: `the five vowels
+    (a, i, u, e, o)`,
+    supplement:`五母音`,
+    answer: ["五母音", "ごぼいん"],
+  },
+  {
+    question: `amount`,
+    supplement:`量、額、総計など`,
+    answer: ["量", "額", "総計", "りょう", "がく", "そうけい"],
+  },
+  {
+    question: `Quantity`,
+    supplement:`量、数など`,
+    answer: ["量", "数", "りょう", "かず"],
+  },
+  {
+    question: `current`,
+    supplement:`読み：ｶﾚﾝﾄ`,
+    answer: ["現在", "げんざい"],
+  },
+  {
+    question: `correct`,
+    supplement:`読み：ｺﾚｸﾄ
+    正しい、修正する`,
+    answer: ["正しい", "修正する", "ただしい", "しゅうせいする"],
   },
 ]
