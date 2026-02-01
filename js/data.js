6a11033cf82adb59d9865c2188f667f0d0ea7ee3
aico0315 <aiko.phoenix12@gmail.com>
2026-02-01 22:50:24 +0900

    addWhiteSpace-preWrap

--- js/data.js
+++ js/data.js
@@ -3,103 +3,108 @@
 export const wordRecords = [
   {
     question: `Average`,
-    supplement:``,
+    supplement:`読み:ｱｳﾞｪﾘｯﾁﾞ`,
     answer: ["平均", "へいきん"],
   },
   {
     question: `Addition`,
-    supplement:``,
+    supplement:`読み:ｱﾃﾞｨｼｮﾝ`,
     answer: ["足し算", "たし算", "たしざん"],
   },
   {
     question: `Subtraction`,
-    supplement:`読み：ｻﾌﾞﾄﾗｸｼｮﾝ`,
+    supplement:`読み:ｻﾌﾞﾄﾗｸｼｮﾝ`,
     answer: ["引き算", "ひき算", "ひきざん"],
   },
   {
     question: `Multiplication`,
-    supplement:`読み：ﾏﾙﾁﾌﾟﾘｹｰｼｮﾝ`,
+    supplement:`読み:ﾏﾙﾁﾌﾟﾘｹｰｼｮﾝ`,
     answer: ["掛け算", "かけ算", "かけざん"],
   },
   {
     question: `Division`,
-    supplement:`読み：ﾃﾞｨｳﾞｨｼﾞｮﾝ`,
+    supplement:`読み:ﾃﾞｨｳﾞｨｼﾞｮﾝ`,
     answer: ["割り算", "わり算", "わりざん"],
   },
   {
     question: `Remainder`,
-    supplement:`読み：ﾘﾒｲﾝﾀﾞｰ
+    supplement:`読み:ﾘﾒｲﾝﾀﾞｰ
     「思い出させる、通知する」はReminder(ﾘﾏｲﾝﾀﾞｰ)
     ※似ているので注意する。`,
     answer: ["割り算の余り", "割り算のあまり", "わり算のあまり", "わりざんの余り", "わりざんのあまり"],
   },
   {
     question: `String`,
-    supplement:`Characterは「文字」混同しないよう注意する`,
+    supplement:`読み:ｽﾄﾘﾝｸﾞ
+    Characterは「文字」混同しないよう注意`,
     answer: ["文字列", "文字れつ", "もじ列", "もじれつ"],
   },
   {
     question: `Character`,
-    supplement:`Stringは「文字列」混同しないよう注意する`,
+    supplement:`読み:ｷｬﾗｸﾀｰ
+    Stringは「文字列」混同しないよう注意する`,
     answer: ["文字", "もじ"],
   },
   {
     question: `Array`,
-    supplement:``,
+    supplement:`読み:ｱﾚｲ`,
     answer: ["配列", "はいれつ"],
   },
   {
     question: `Subject`,
-    supplement:`件名、科目、教科など`,
+    supplement:`読み:ｻﾌﾞｼﾞｪｸﾄ
+    件名、科目、教科など`,
     answer: ["件名", "科目", "教科", "けんめい", "かもく", "きょうか"],
   },
   {
     question: `Semantics`,
-    supplement:`読み：ｾﾏﾝﾃｨｸｽ
+    supplement:`読み:ｾﾏﾝﾃｨｸｽ
     内容や意図、動作など`,
     answer: ["内容", "意図", "動作", "ないよう", "いと", "どうさ"],
   },
   {
     question: `Syntax`,
-    supplement:`形式やルール、書き方など`,
+    supplement:`読み:ｼﾝﾀｯｸｽ
+    形式やルール、書き方など`,
     answer: ["形式", "ルール", "書き方", "けいしき", "るーる", "かきかた"],
   },
   {
     question: `delimiter`,
-    supplement:`読み：ﾃﾞﾐﾘﾀ
+    supplement:`読み:ﾃﾞﾐﾘﾀ
     形式やルール、書き方など`,
     answer: ["区切り文字", "区切り記号", "区切りもじ", "くぎり文字", "区切り記号", "区切りきごう", "くぎり記号"],
   },
   {
     question: `specified`,
-    supplement:`読み：ｽﾍﾟｼﾌｧｲﾄﾞ
-    指定された、明記された`,
+    supplement:`読み:ｽﾍﾟｼﾌｧｲﾄﾞ
+    指定された、明記されたなど`,
     answer: ["指定された", "明記された", "していされた", "めいきされた"],
   },
   {
     question: `integer`,
-    supplement:`整数`,
+    supplement:`読み:ｲﾝﾃｼﾞｬｰ`,
     answer: ["整数", "せいすう"],
   },
   {
     question: `vowel`,
-    supplement:`母音`,
+    supplement:`読み:ｳﾞｧｳｧﾙ`,
     answer: ["母音", "ぼいん"],
   },
   {
-    question: `the five vowels
-    (a, i, u, e, o)`,
-    supplement:`五母音`,
+    question: `the five vowels`,
+    supplement:`(a, i, u, e, o)の意味`,
     answer: ["五母音", "ごぼいん"],
   },
   {
     question: `amount`,
-    supplement:`量、額、総計など`,
+    supplement:`読み:ｱﾏｳﾝﾄ
+    量、額、総計など`,
     answer: ["量", "額", "総計", "りょう", "がく", "そうけい"],
   },
   {
     question: `Quantity`,
-    supplement:`量、数など`,
+    supplement:`読み:ｸｫﾝﾃｨﾃｨ
+    量、数など`,
     answer: ["量", "数", "りょう", "かず"],
   },
   {
